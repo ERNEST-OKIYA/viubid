efae60bbff3adb664590e363231b9331610542b1
Joshua Abuga <joshuabuga@gmail.com>
2020-07-20 03:49:48 +0300

    updates

--- ussd/screens/utils.py
+++ ussd/screens/utils.py
@@ -5,7 +5,7 @@ class Fetcher:
 
 	def fetch_bids_menu(self):
 		ls = []
-		menu = Bid.objects.filter(is_open=True).order_by('-closes_at').values_list('product__name','code','product')
+		menu = Bid.objects.filter(is_open=True).order_by('-priority').values_list('product__name','code','product')
 		ls.extend(((str(i), x) for i,x in enumerate(menu, 1)))
 		ls = OrderedDict(ls)
 		ls[str(len(ls)+1)] = ('How it works', 'sm.how_it_works')
