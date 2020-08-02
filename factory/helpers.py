d73a662bf17e0e3dbd63525bdf04964b692da657
Joshua Abuga <joshuabuga@gmail.com>
2020-08-02 06:08:51 +0300

    updates

--- factory/helpers.py
+++ factory/helpers.py
@@ -428,7 +428,7 @@ class Helpers:
             match = difflib.get_close_matches(value.upper(),lookups,n=1)
             print(match)
             if len(match)!=0:
-                bid = Bid.objects.filter(lookups__contains=match)
+                bid = Bid.objects.filter(lookups__contained_by=match).last()
                 print(bid)
                 return bid.code
 
