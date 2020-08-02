5b998920db1b63b894902569ef1e34e75cd9f4da
Joshua Abuga <joshuabuga@gmail.com>
2020-08-02 04:37:04 +0300

    updates

--- scripts/resolve_bids.py
+++ scripts/resolve_bids.py
@@ -57,7 +57,7 @@ def run():
                         if len(amount)!=0:
                             amount = amount[0]
                         transaction_amount = 20
-                        payin = PayIn.objects.filter(bill_reference_number=bill_ref_no,msisdn=bid.user.phone_number).last()
+                        payin = PayIn.objects.filter(bill_reference_number=bill_ref_no,msisdn=bid.bid_entry.user.phone_number).last()
                         transaction_id = payin.transaction_id
                         print("bid resolved")
                         # helpers.create_bid_entry(bid.user,amount,transaction_id,code,source,bill_ref_no,transaction_amount)
