b627da6fcff1bf136f5b361b468470ee16162c31
Joshua Abuga <joshuabuga@gmail.com>
2020-12-06 20:34:20 +0300

    updates

--- scripts/get_uniquebids.py
+++ scripts/get_uniquebids.py
@@ -8,7 +8,7 @@ def get_uniques(bid_id):
     unique_bids = UserBid.objects.filter(bid_entry__bid__id=bid_id).values('amount').annotate(amount_count=Count('amount')).filter(amount_count__lt=2).order_by('amount')
     for ub in unique_bids:
         amount = ub.get('amount')
-        entry = UserBid.objects.get(amount=amount)
+        entry = UserBid.objects.filter(bid_entry__bid__id=bid_id).get(amount=amount)
         print(entry.bid_entry.bid.user.profile.first_name,entry.bid_entry.bid.user.profile.last_name,entry.bid_entry.bid.code,entry.bid_entry.bid.product.name,entry.amount)
 
 
