46f77fa4ec5e52ddeda076372e9505122c7bfbe7
Joshua Abuga <joshuabuga@gmail.com>
2020-12-06 20:19:48 +0300

    updates

--- scripts/get_uniquebids.py
+++ scripts/get_uniquebids.py
@@ -5,9 +5,10 @@ from django.db.models import Count
 
 def get_uniques(bid_id):
 
-    unique_bids = UserBid.objects.filter(bid_entry__bid__id=bid_id).annotate(amount_count=Count('amount')).filter(amount_count__lt=2)
+    unique_bids = UserBid.objects.filter(bid_entry__bid__id=bid_id).values('amount').annotate(amount_count=Count('amount')).filter(amount_count__lt=2)
     for entry in unique_bids:
-        print(entry.bid_entry.user.profile.first_name,entry.bid_entry.user.profile.last_name,entry.bid_entry.bid.code,entry.bid_entry.bid.product.name,entry.amount)
+        print(entry)
+        # print(entry.bid_entry.bid.user.profile.first_name,entry.bid_entry.bid.user.profile.last_name,entry.bid_entry.bid.code,entry.bid_entry.bid.product.name,entry.amount)
 
 
 
